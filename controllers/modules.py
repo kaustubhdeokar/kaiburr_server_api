12f9c109b36042391393a066875712eea518394e
apurva <apuayush@gmail.com>
2019-02-22 11:32:12 +0530

    added Procfile

--- controllers/modules.py
+++ controllers/modules.py
@@ -6,6 +6,7 @@ from tornado.gen import coroutine
 
 # other libraries
 import json
+import os
 from motor import MotorClient
 
 # env
@@ -15,6 +16,7 @@ try:
     db_uname = env.db
     db_pswd = env.pswd
 except:
-    pass
+    db_uname = os.environ['db_name']
+    db_pswd = os.environ['db_pswd']
 
 db = MotorClient("mongodb://{0}:{1}@ds147225.mlab.com:47225/kalburr-server".format(db_uname, db_pswd))["kalburr-server"]
